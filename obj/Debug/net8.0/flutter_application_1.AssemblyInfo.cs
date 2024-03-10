e2661e7eec5f92e54fecea2535318e7fe5326880
fade697 <132776309+fade697@users.noreply.github.com>
2024-03-10 20:18:32 +0200

    data

--- obj/Debug/net8.0/flutter_application_1.AssemblyInfo.cs
+++ obj/Debug/net8.0/flutter_application_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("flutter_application_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe664de799cc2c392a955535c9e135ee7afb3f4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8796f862d14e64f390b60890735da6b7983d1376")]
 [assembly: System.Reflection.AssemblyProductAttribute("flutter_application_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("flutter_application_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
